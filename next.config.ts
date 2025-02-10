ead6d0884c3863924549560882b9255f78d70b4d
J-i-w-o-o <andrielgeomergabriel@gmail.com>
2025-02-11 04:53:50 +0800

    test deploy

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
-const nextConfig = {
-  images: { unoptimized: true }, // Cloudflare does not support Next.js image optimization yet
-};
+import type { NextConfig } from "next";
 
-export default nextConfig;
+const nextConfig = {
+    output: "export", // Enables static export
+    basePath: "/MyBlogSpot", // Replace with your GitHub repo name
+    images: {
+      unoptimized: true, // GitHub Pages does not support Next.js image optimization
+    },
+  };
+  
+  export default nextConfig;
+  
