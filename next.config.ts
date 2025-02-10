d9f668e0b1b95a040be459d7258ce575b61e934d
J-i-w-o-o <andrielgeomergabriel@gmail.com>
2025-02-11 04:55:41 +0800

    test deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-    output: "export", // Enables static export
-    basePath: "/MyBlogSpot", // Replace with your GitHub repo name
     images: {
       unoptimized: true, // GitHub Pages does not support Next.js image optimization
     },
